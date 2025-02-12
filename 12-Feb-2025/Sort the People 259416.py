1c1c3b567a413fcc5e90ea39b4677d600c7a2155
yobernu <yobernu@gmail.com>
2025-02-12 14:51:50 +0300

    Update Sort the People 259416.py

--- 12-Feb-2025/Sort the People 259416.py
+++ 12-Feb-2025/Sort the People 259416.py
@@ -2,12 +2,15 @@
 
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        #if we have same heights and same names in a class
         # for i in range(len(names)):
         #     for j in range(i+1, len(names)):
         #         if heights[j] > heights[i]:
         #             heights[j], heights[i] = heights[i], heights[j]
         #             names[j], names[i] = names[i], names[j]
         # return names
+
+        # if we ahve distnict height in a class
         my_dict = defaultdict()
         sorted_height = heights[:]
         sorted_height = sorted(sorted_height)
