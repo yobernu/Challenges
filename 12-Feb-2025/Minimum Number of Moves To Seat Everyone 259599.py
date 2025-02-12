d2ed54920fcff00b6c646de14aa0e3bed5a0af69
yobernu <yobernu@gmail.com>
2025-02-12 14:49:59 +0300

    Created 'Minimum Number of Moves To Seat Everyone' by A2SV Hub

---
+++ 12-Feb-2025/Minimum Number of Moves To Seat Everyone 259599.py
@@ -0,0 +1,10 @@
+# Problem: Minimum Number of Moves To Seat Everyone - https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats = sorted(seats)
+        students = sorted(students)
+        ans = 0
+        for i in range(len(seats)):
+            ans += abs(seats[i] - students[i])
+        return ans
