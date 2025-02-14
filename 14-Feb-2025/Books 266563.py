0974020766e017eee126c85dc92fefd5c8246c95
yobernu <yobernu@gmail.com>
2025-02-14 22:05:19 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 266563.py
@@ -0,0 +1,14 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+minutes = list(map(int, input().split()))
+
+left, total, ans = 0, 0, 0
+for right in range(n):
+    total += minutes[right] 
+    while total > t:  
+        total -= minutes[left]
+        left += 1
+    ans = max(ans, right - left + 1)
+
+print(ans)
