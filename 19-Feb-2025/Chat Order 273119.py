034fc59488ffc82245e73a62a33d2409a816b8ca
yobernu <yobernu@gmail.com>
2025-02-19 10:59:46 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 19-Feb-2025/Chat Order 273119.py
@@ -0,0 +1,15 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+persons = []
+for _ in range(n):
+    p = input()
+    persons.append(p)
+# persons[::-1]
+rev = persons[::-1]
+s = set()
+for person in rev:
+    if person not in s:
+        s.add(person)
+        print(person)
+
