541673a9d5cc8532d64493f4fdd16b5412999743
yobernu <yobernu@gmail.com>
2025-02-22 21:32:39 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280467.py
@@ -0,0 +1,15 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+row = 0
+for i in range(1, 6):
+    arr = list(map(int, input().split()))
+    if sum(arr) == 1:
+        row = i
+    matrix.append(arr)
+col = 0
+for i in range(1, 6):
+    if matrix[row-1][i-1] == 1:
+        col = i
+ans = abs(row - 3) + abs(col-3)
+print(ans)
