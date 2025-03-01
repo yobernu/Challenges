85485f54c851a16ce727c227b30c6d0ecd8ec656
yobernu <yobernu@gmail.com>
2025-03-01 20:56:42 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/F - Covered Points Count 287734.py
@@ -0,0 +1,26 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+from collections import defaultdict
+
+n = int(input())
+dic = defaultdict(int)
+
+for _ in range(n):
+    a = list(map(int, input().split()))
+    dic[a[0]] += 1
+    dic[a[1] + 1] -= 1
+
+sorted_keys = sorted(dic.keys())
+count = 0
+active_intervals = defaultdict(int)
+
+for i in range(len(sorted_keys) - 1): 
+    count += dic[sorted_keys[i]]
+    active_intervals[count] += sorted_keys[i + 1] - sorted_keys[i]
+
+result = [0] * n
+for k, v in active_intervals.items():
+    if k > 0: 
+        result[k - 1] = v
+
+print(*result)
