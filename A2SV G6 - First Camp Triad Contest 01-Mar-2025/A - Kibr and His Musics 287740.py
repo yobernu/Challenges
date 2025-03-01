bac8e218db52be01d1868c2e1f5a0e1b61202d00
yobernu <yobernu@gmail.com>
2025-03-01 20:57:34 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/A - Kibr and His Musics 287740.py
@@ -0,0 +1,19 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+n , k = map(int, input().split())
+arr = []
+for _ in range(n):
+    c , t = map(int , input().split())
+    arr.append(c*t)
+ 
+for i in range(1, n):
+    arr[i] +=  arr[i-1]
+ 
+ 
+nums = list(map(int, input().split()))
+ 
+i = 0
+for q in nums:
+    while i < n and arr[i] < q:
+        i += 1
+    print(i + 1)
