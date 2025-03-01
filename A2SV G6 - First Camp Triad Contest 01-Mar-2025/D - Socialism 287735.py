babc7610fd8730c37ef496571750f7e9ccc0ab6e
yobernu <yobernu@gmail.com>
2025-03-01 20:56:45 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/D - Socialism 287735.py
@@ -0,0 +1,15 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, min_wealth = map(int, input().split())
+    lst = list(map(int, input().split()))
+    lst.sort(reverse=True)
+    
+    count, tot = 0, 0
+    for i in range(n):
+        tot += lst[i]
+        if tot/(i+1) < min_wealth:
+            break
+        count = i+1
+    print(count)
