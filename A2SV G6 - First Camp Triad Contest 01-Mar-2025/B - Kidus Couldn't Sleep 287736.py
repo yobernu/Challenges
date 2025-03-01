5fede35e0037e4ac53a0ae4d7701864800102699
yobernu <yobernu@gmail.com>
2025-03-01 20:56:54 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/B - Kidus Couldn't Sleep 287736.py
@@ -0,0 +1,13 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n , k = map(int , input().split())
+arr = list(map(int, input().split()))
+ 
+
+window_sum = sum(arr[:k])
+tot = window_sum
+
+for i in range(k, n):
+    window_sum += arr[i] - arr[i-k]
+    tot += window_sum
+print(tot/(n-k+1))
