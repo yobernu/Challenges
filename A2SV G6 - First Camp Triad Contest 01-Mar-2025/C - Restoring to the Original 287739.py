ea8a1652d69a509ad2b1645c5395be4f2eb6847a
yobernu <yobernu@gmail.com>
2025-03-01 20:57:27 +0300

    Created 'C - Restoring to the Original' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/C - Restoring to the Original 287739.py
@@ -0,0 +1,19 @@
+# Problem: C - Restoring to the Original - https://codeforces.com/gym/589822/problem/C
+
+n = int(input())
+s = input()
+ones = 0
+zeros = 0
+for i in s:
+    if i == "n":
+        ones += 1
+    elif i == "z":
+        zeros += 1
+ans = []
+if ones:
+    for i in range(ones):
+        ans.append(1)
+if zeros:
+    for i in range(zeros):
+        ans.append(0)
+print(*ans)
