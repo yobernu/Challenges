78ed07b6b8bd417addfdd05183f056d1f5dcdc27
yobernu <yobernu@gmail.com>
2025-03-01 20:57:01 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 01-Mar-2025/I - The Odd Challenge 287737.py
@@ -0,0 +1,32 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+t = int(input())
+lst = []
+for _ in range(t):
+    n, q = map(int, input().split())
+    arr = list(map(int, input().split()))
+    for i in range(1, len(arr)):
+        arr[i] += arr[i-1]
+    # print(arr)
+    for _ in range(q):
+        l = list(map(int, input().split()))
+        left = l[0]
+        right = l[1]
+        left -= 1
+        right -= 1
+        
+        tot = arr[-1]
+        if left == 0:
+            new_tot = tot - (arr[right]) + (right - left+1)*l[2]
+        else:
+            new_tot = tot - (arr[right] - arr[left - 1]) + (right - left+1)*l[2]
+        # print(arr, tot,  new_tot)
+        if new_tot%2 != 0:
+            print("YES")
+        else:
+            print("NO")
+        
+    # print("------second case--------")
+
+
+
