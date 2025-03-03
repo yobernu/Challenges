a3f4d6884165287932117f4fba678c4945749c2f
yobernu <yobernu@gmail.com>
2025-03-04 00:51:28 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 03-Mar-2025/Karen and Coffee 290350.py
@@ -0,0 +1,20 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n, k, q = map(int, input().split())
+p = [0]*200002
+rn = [0]*200002
+for _ in range(n):
+    l, r = map(int, input().split())
+    p[l] += 1
+    p[r+1] -= 1
+for i in range(1, len(p)):
+    p[i] += p[i-1]
+    if p[i] >= k:
+        rn[i] += rn[i-1] + 1
+    else:
+        rn[i] += rn[i-1]
+
+for _ in range(q):
+    a, b = map(int, input().split())
+    print(rn[b] - rn[a-1])
+    
