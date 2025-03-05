133a024fed0527f55a8f6390311c5ef5ff753883
yobernu <yobernu@gmail.com>
2025-03-05 17:32:18 +0300

    Created 'Can Place Flowers' by A2SV Hub

---
+++ 05-Mar-2025/Can Place Flowers 293002.py
@@ -0,0 +1,15 @@
+# Problem: Can Place Flowers - https://leetcode.com/problems/can-place-flowers/
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        count = 0
+        
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
+                count += 1
+                if count >= n:
+                    return True
+        
+        return count >= n
