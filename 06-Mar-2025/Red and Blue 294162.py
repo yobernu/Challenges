bcc5ef7b45e2c39f7a8e9ab5e4956d5f6b821e98
yobernu <yobernu@gmail.com>
2025-03-06 14:21:55 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 06-Mar-2025/Red and Blue 294162.py
@@ -0,0 +1,15 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    red = list(map(int, input().split()))
+    m = int(input())
+    blue = list(map(int, input().split()))
+
+    ans = 0
+    for i in range(1, n):
+        red[i] += red[i-1]
+    for i in range(1, m):
+        blue[i] += blue[i-1]
+    print(max(0, max(red), max(blue), max(red) + max(blue)))
