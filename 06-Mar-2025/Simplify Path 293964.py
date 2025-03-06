29cafbbfbced178add796632b413bfd2dd4240f0
yobernu <yobernu@gmail.com>
2025-03-06 10:58:26 +0300

    Created 'Simplify Path' by A2SV Hub

---
+++ 06-Mar-2025/Simplify Path 293964.py
@@ -0,0 +1,15 @@
+# Problem: Simplify Path - https://leetcode.com/problems/simplify-path/
+
+class Solution(object):
+    def simplifyPath(self, path):
+        stack = []
+        directories = path.split("/")
+        for dir in directories:
+            if dir == "." or not dir:
+                continue
+            elif dir == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(dir)
+        return "/" + "/".join(stack)
