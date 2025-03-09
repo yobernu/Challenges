601ba559e3efd941df9c4eef4698f64fab9835c5
yobernu <yobernu@gmail.com>
2025-03-09 23:21:42 +0300

    Created 'Segments with Small Spread' by A2SV Hub

---
+++ 09-Mar-2025/Segments with Small Spread 299052.py
@@ -0,0 +1,30 @@
+# Problem: Segments with Small Spread - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/F
+
+from collections import deque
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+
+max_dq = deque()  
+min_dq = deque()  
+
+left = 0
+res = 0
+
+for right in range(len(a)):
+    while max_dq and a[max_dq[-1]] < a[right]:
+        max_dq.pop()
+    max_dq.append(right)
+
+    while min_dq and a[min_dq[-1]] > a[right]:
+        min_dq.pop()
+    min_dq.append(right)
+
+    while a[max_dq[0]] - a[min_dq[0]] > k:
+        left += 1  
+        if max_dq[0] < left:
+            max_dq.popleft()
+        if min_dq[0] < left:
+            min_dq.popleft()
+    # print(max_dq, min_dq)
+    res += right - left + 1
+print(res)
