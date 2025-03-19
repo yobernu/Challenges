340cf3e37a506d82c2242fd47f17757486ff918b
yobernu <yobernu@gmail.com>
2025-03-19 06:12:59 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 19-Mar-2025/A - Free Ice Cream for the Kids 312023.py
@@ -0,0 +1,15 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n,x = map(int, input().split())
+dc = 0
+tot = x
+for _ in range(n):
+    a,b = map(str, input().split())
+    if a == "+":
+        tot += int(b)
+    else:
+        if tot < int(b):
+            dc += 1
+        else:
+            tot -= int(b)
+print(tot, dc)
