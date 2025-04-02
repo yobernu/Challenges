2737cdb4a6a330901c6f06f9353518e576d522f1
yobernu <yobernu@gmail.com>
2025-04-02 17:40:52 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 02-Apr-2025/Missing Number 323957.py
@@ -0,0 +1,7 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for num in range(len(nums) + 1):
+            if num not in nums:
+                return num
