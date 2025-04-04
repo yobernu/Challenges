0210147051cfbc8725f2a3fb9f9af2e96b3dc61b
yobernu <yobernu@gmail.com>
2025-04-05 00:38:03 +0300

    Created 'B - Increasing Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #7 04-Apr-2025/B - Increasing Sequence 326580.py
@@ -0,0 +1,12 @@
+# Problem: B - Increasing Sequence - https://codeforces.com/gym/596141/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    last_elem = 0
+    for num in a:
+        last_elem += 1
+        if last_elem == num:
+            last_elem += 1
+    print(last_elem)
