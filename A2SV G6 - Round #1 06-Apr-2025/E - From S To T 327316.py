6b2c040a20043871194beb2f7048849d35ad40b9
yobernu <yobernu@gmail.com>
2025-04-06 19:25:02 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 06-Apr-2025/E - From S To T 327316.py
@@ -0,0 +1,24 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+from collections import Counter
+
+t = int(input())
+for _ in range(t):
+    s, t_str, p = input(), input(), input()
+    idx = 0
+    cou = Counter(p)
+    flag = False
+
+    for ch in t_str:
+        if idx < len(s) and ch == s[idx]:
+            idx += 1
+        elif cou[ch] > 0:
+            cou[ch] -= 1
+        else:
+            flag = True
+            break
+
+    if flag or idx < len(s):
+        print("NO")
+    else:
+        print("YES")
