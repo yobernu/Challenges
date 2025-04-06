89e950693ebe8f5a2cc3648c03c2de610a660a6a
yobernu <yobernu@gmail.com>
2025-04-06 18:18:45 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 06-Apr-2025/D - Repeating Cipher 327291.py
@@ -0,0 +1,13 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n = int(input())
+word = input()
+count = 1
+ans = ""
+l = 0
+while l < n:
+    ans += word[l]
+    l += count
+    count += 1
+    
+print(ans)
