052321c071afb544c0919b66f7918df884153dfc
yobernu <yobernu@gmail.com>
2025-05-05 20:42:59 +0300

    Created 'Furthest Building You Can Reach' by A2SV Hub

---
+++ 05-May-2025/Furthest Building You Can Reach 348746.py
@@ -0,0 +1,18 @@
+# Problem: Furthest Building You Can Reach - https://leetcode.com/problems/furthest-building-you-can-reach/
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights)-1):
+            diff = heights[i+1] - heights[i]
+            if diff > 0:
+                heapq.heappush(heap, diff)
+            if len(heap) > ladders:
+                smallest_jump = heapq.heappop(heap)
+                bricks -= smallest_jump
+
+            if bricks < 0:
+                return i
+
+        return len(heights) - 1
+            
