25caebb254b737fdc53cba57e04a73593507d99a
yobernu <yobernu@gmail.com>
2025-05-12 19:58:09 +0300

    Created 'Spanning Tree' by A2SV Hub

---
+++ 12-May-2025/Spanning Tree 353850.py
@@ -0,0 +1,43 @@
+# Problem: Spanning Tree - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/E
+
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.size = [1] * size
+
+    def find(self, x):
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.size[rootX] > self.size[rootY]:
+                self.root[rootY] = rootX
+                self.size[rootX] += self.size[rootY]
+            else:
+                self.root[rootX] = rootY
+                self.size[rootY] += self.size[rootX]
+            return True
+        return False
+
+n, m = map(int, input().split())
+edges = []
+
+for _ in range(m):
+    u, v, w = map(int, input().split())
+    edges.append((w, u - 1, v - 1)) 
+edges.sort()
+uf = UnionFind(n)
+res = 0
+edges_used = 0
+for w, u, v in edges:
+    if uf.union(u, v): 
+        res += w
+        edges_used += 1
+        if edges_used == n - 1:
+            break
+print(res)
