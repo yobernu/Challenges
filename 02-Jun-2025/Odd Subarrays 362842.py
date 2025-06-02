8e07cc83bac1daa1ded54eabc2f73bbe26a1a88f
yobernu <yobernu@gmail.com>
2025-06-02 20:18:12 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 02-Jun-2025/Odd Subarrays 362842.py
@@ -0,0 +1,18 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+from collections import deque
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    dq = deque()
+    count = 0
+    for i in range(n):
+        if dq and dq[-1] > a[i]:
+            dq = deque()
+            count += 1
+        else:
+            if dq:
+                dq.popleft()
+            dq.append(a[i])
+    print(count)
