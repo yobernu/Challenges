5633c9461a9d5e0a05faf4f61a78f9faff313da4
yobernu <yobernu@gmail.com>
2025-06-04 23:11:33 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 04-Jun-2025/Cirno's Perfect Bitmasks Classroom 363777.py
@@ -0,0 +1,14 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+def solve():
+    x = int(input())
+    y = x & -x
+
+    if x ^ y > 0:
+        print(y)
+    else:
+        print(y + (~x & (x + 1)))  
+                                          
+t = int(input())  
+for _ in range(t):
+    solve()
