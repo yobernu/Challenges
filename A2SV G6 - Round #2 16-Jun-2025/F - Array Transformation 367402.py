b5b654d2cac8fbecb5deb0c82237f54fa9a923df
yobernu <yobernu@gmail.com>
2025-06-17 01:07:30 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 16-Jun-2025/F - Array Transformation 367402.py
@@ -0,0 +1,19 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+def solve(arr, n):
+    freq1 = Counter(arr)
+    opt = list(freq1.values())
+    opt.sort()
+    res = float('inf')
+    for i in range(len(opt)):
+        rem = n - (len(opt) - i) * opt[i]
+        res = min(res, rem)
+
+    return res
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(solve(arr, n))
