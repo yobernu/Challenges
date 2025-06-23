0aa909d88fab5bd5bd6106929f49f6ddeb409283
yobernu <yobernu@gmail.com>
2025-06-24 00:45:05 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 23-Jun-2025/Destroying Bridges 369699.py
@@ -0,0 +1,9 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    if k > n-2:
+        print(1)
+    else:
+        print(n)
