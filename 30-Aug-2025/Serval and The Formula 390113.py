0c6b7a4c1aa98021fae3a2915c9dffadb3412671
Yoseph Berhanu <yobernu@gmail.com>
2025-08-30 13:22:12 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 30-Aug-2025/Serval and The Formula 390113.py
@@ -0,0 +1,15 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+import sys
+
+def solve():
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        x, y = map(int, sys.stdin.readline().split())
+        if x == y:
+            print(-1)
+            continue
+        else:
+            k = 2**48 - max(x, y)
+            print(k)
+solve()
