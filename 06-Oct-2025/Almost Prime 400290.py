bd11c8614dbdf0f079db39f716992b9a97bc3ea9
Yoseph Berhanu <yobernu@gmail.com>
2025-10-06 19:20:42 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 06-Oct-2025/Almost Prime 400290.py
@@ -0,0 +1,23 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+from collections import Counter;
+def almostprime(n):
+    factors = []
+    d = 2
+    while d*d <= n:
+        while n%d == 0:
+            factors.append(d)
+            n//=d
+        d += 1
+    if n > 1: factors.append(n)
+    
+    counter = Counter(factors)
+    return len(counter) == 2
+
+num = int(input())
+ans = 0
+for i in range(1, num+1):
+    if almostprime(i):
+        ans += 1
+print(ans)
+    
