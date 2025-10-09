543b5dae7a4f8889e01fc0821fdebb511fd0f446
Yoseph Berhanu <yobernu@gmail.com>
2025-10-09 18:09:36 +0300

    Created 'Row GCD' by A2SV Hub

---
+++ 09-Oct-2025/Row GCD 401507.py
@@ -0,0 +1,14 @@
+# Problem: Row GCD - https://codeforces.com/problemset/problem/1458/a
+
+from math import gcd
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+g = 0
+for i in range(1, n):
+    g = gcd(g, abs(a[0] - a[i]))
+
+ans = [gcd(a[0] + bj, g) for bj in b]
+print(' '.join(map(str, ans)))
