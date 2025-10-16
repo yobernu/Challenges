d733eb0f208f11e00668cad9a92cc6ad0137e622
Yoseph Berhanu <yobernu@gmail.com>
2025-10-16 22:52:59 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 16-Oct-2025/Serval and The Formula 403843.py
@@ -0,0 +1,15 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+getint = lambda: int(input())
+getints = lambda: map(int, input().split())
+
+def solve():
+	x, y = getints()
+	if x == y:
+		print(-1)
+	else:
+		print(2 ** 48 - max(x, y))
+
+t = getint()
+for _ in range(t):
+	solve()
