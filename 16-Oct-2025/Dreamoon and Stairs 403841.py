c30ffdf38a6286e8bbf822bfcc21c8a95c625ab0
Yoseph Berhanu <yobernu@gmail.com>
2025-10-16 22:52:14 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 16-Oct-2025/Dreamoon and Stairs 403841.py
@@ -0,0 +1,15 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+n, m = map(int, input().split())
+
+min_moves = (n + 1) // 2
+answer = -1
+
+if n < m:
+    print(answer)
+else:
+    for i in range(min_moves, n + 1):  # include n
+        if i % m == 0:
+            answer = i
+            break
+    print(answer)
