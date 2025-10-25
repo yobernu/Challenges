372791c20aab51a37d785fe2a41f8e6e89d616bd
Yoseph Berhanu <yobernu@gmail.com>
2025-10-25 23:30:58 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 25-Oct-2025/F - Binary Substrings with Exactly k Ones 405742.py
@@ -0,0 +1,23 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+k = int(input())
+nums = input()
+n = len(nums)
+
+def atmostk(k):
+    if k == -1:
+        return 0
+    left = 0 
+    count = 0
+    ans = 0
+    for right in range(n):
+        if nums[right] == "1":
+            count += 1
+        while count > k:
+            if nums[left] == "1":
+                count -= 1
+            left += 1 
+        ans += right - left + 1
+    
+    return ans 
+print(atmostk(k) - atmostk(k-1)) 
