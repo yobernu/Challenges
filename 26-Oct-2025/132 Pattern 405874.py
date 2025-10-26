616a8747c72c1644b3c69e6d77e01c49591994cd
Yoseph Berhanu <yobernu@gmail.com>
2025-10-27 00:12:20 +0300

    Created '132 Pattern' by A2SV Hub

---
+++ 26-Oct-2025/132 Pattern 405874.py
@@ -0,0 +1,14 @@
+# Problem: 132 Pattern - https://leetcode.com/problems/132-pattern/
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack=[]
+        minn=nums[0]
+        for i in nums[1:]:
+            while stack and i>=stack[-1][0]:
+                stack.pop()
+            if stack and i > stack[-1][1]:
+                return True
+            stack.append([i,minn])
+            minn=min(minn,i)
+        return False
