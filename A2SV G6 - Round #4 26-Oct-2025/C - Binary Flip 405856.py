d04dfeb95e0cb4fed6a3e87e84639b84ad116e7e
Yoseph Berhanu <yobernu@gmail.com>
2025-10-26 23:31:05 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Oct-2025/C - Binary Flip 405856.py
@@ -0,0 +1,29 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+import sys
+
+def solve():
+    n = int(input())
+    A = input()
+    B = input()
+    
+    A += '0'
+    B += '0'
+    count = 0
+    
+    for i in range(n):
+        if A[i] == '1':
+            count += 1
+        else:
+            count -= 1
+        
+        if ((A[i] == B[i]) != (A[i + 1] == B[i + 1])) and count != 0:
+            print("NO")
+            return
+    
+    print("YES")
+
+
+t = int(input())
+for _ in range(t):
+    solve()
