b643aca09a6778ab5c151bd65755560d9e8c2380
Yoseph Berhanu <yobernu@gmail.com>
2025-10-26 23:31:01 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Oct-2025/D - Minimize the Distance 405855.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+points = list(map(int, input().split()))
+points.sort() 
+
+if n % 2 == 0:
+    median = points[n // 2 - 1]
+else:
+    median = points[n // 2] 
+print(median)
