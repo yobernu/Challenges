27475b62547fa3671bfc66132fc1c7beabe278fc
Yoseph Berhanu <yobernu@gmail.com>
2025-10-26 23:31:31 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Oct-2025/A - A+ Student 405859.py
@@ -0,0 +1,21 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    arr = []
+    if a > max(b, c):
+        arr.append(0)
+    else:
+        arr.append(max(b, c) - a+1)
+    if b > max(a, c):
+        arr.append(0)
+    else:
+        arr.append(max(a, c) - b+1)
+    if c > max(a, b):
+        arr.append(0)
+    else:
+        arr.append(max(a, b) - c+1)
+    print(*arr)
+
