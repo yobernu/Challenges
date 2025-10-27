5057d9896105850bf045d9af7a6bd67435c47c5e
Yoseph Berhanu <yobernu@gmail.com>
2025-10-27 22:36:07 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 27-Oct-2025/B - Fix the Forecast! 406040.py
@@ -0,0 +1,22 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, k =  map(int, input().split())
+    a =  list(map(int, input().split()))
+    b =  list(map(int, input().split()))
+    
+    # Pair forecast values with indices
+    a = [(a[i], i) for i in range(n)]
+    
+    # Sort both arrays
+    a.sort()
+    b.sort()
+    
+    # Rearrange elements in the correct order
+    ans = [0] * n
+    for i in range(n):
+        _, ind = a[i]
+        ans[ind] = b[i]
+    
+    print(*ans)
