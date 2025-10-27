a70158d2016273ebab7950c5377222a9e203f582
Yoseph Berhanu <yobernu@gmail.com>
2025-10-27 22:36:25 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 27-Oct-2025/C - Escape-Proof Transfers 406042.py
@@ -0,0 +1,17 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = map(int, input().split())
+a = list(map(int, input().split()))
+
+count = 0
+l = 0
+for num in a:
+    if num <= t:
+        l += 1
+    else:
+        l = 0
+    
+    if l >= c:
+        count += 1
+
+print(count)
