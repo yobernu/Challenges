20fa9803a25d9cdb26ce686811d51618d28cacd0
Yoseph Berhanu <yobernu@gmail.com>
2025-10-27 22:36:33 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 27-Oct-2025/A - Language Identifier 406043.py
@@ -0,0 +1,12 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t = int(input())
+for _ in range(t):
+    word = input()
+    n = len(word)
+    if word[n-2:] == "po":
+        print("FILIPINO")
+    elif word[n-4:] == "desu" or word[n-4:] == "masu":
+        print("JAPANESE")
+    elif word[n-5:] == "mnida":
+        print("KOREAN")
