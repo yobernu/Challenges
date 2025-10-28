26115254fbe1c8dc25fdef2eb5efb442cec88876
Yoseph Berhanu <yobernu@gmail.com>
2025-10-28 09:33:42 +0300

    Created 'Reveal Cards In Increasing Order' by A2SV Hub

---
+++ 28-Oct-2025/Reveal Cards In Increasing Order 406086.py
@@ -0,0 +1,16 @@
+# Problem: Reveal Cards In Increasing Order - https://leetcode.com/problems/reveal-cards-in-increasing-order/
+
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        n = len(deck)
+        newArr = [0] * n
+        sor = deque(sorted(deck))
+        jump = False
+        while sor:
+            for i in range(n):
+                if newArr[i] == 0:
+                    if not jump:
+                        newArr[i] = sor[0]
+                        sor.popleft()
+                    jump = not jump
+        return newArr
