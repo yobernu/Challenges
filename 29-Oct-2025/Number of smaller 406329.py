c1ca4ad6faf0982e96589b69da80d259da80e289
Yoseph Berhanu <yobernu@gmail.com>
2025-10-29 21:36:06 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 29-Oct-2025/Number of smaller 406329.py
@@ -0,0 +1,26 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+
+
+import sys
+from collections import defaultdict
+
+
+def solve():
+    data = sys.stdin.read().splitlines()
+    n, m = map(int, data[0].split())
+
+    arr1 = list(map(int, data[1].split()))
+    arr2 = list(map(int, data[2].split()))
+    
+    ans = []
+    j = 0
+    count = 0
+    for num in arr2:
+        while j < n and num > arr1[j]:
+            count += 1
+            j += 1
+        ans.append(count)
+    print(*ans)
+solve()
+        
