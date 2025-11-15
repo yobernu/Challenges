81d872fc344368282c120e7c0352ef06ba7f352c
Yoseph Berhanu <yobernu@gmail.com>
2025-11-15 16:10:01 +0300

    Created 'Reorder List' by A2SV Hub

---
+++ 15-Nov-2025/Reorder List 408938.py
@@ -0,0 +1,35 @@
+# Problem: Reorder List - https://leetcode.com/problems/reorder-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, curr = None, slow.next
+        slow.next = None  
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        second = prev 
+        first = head
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+        
