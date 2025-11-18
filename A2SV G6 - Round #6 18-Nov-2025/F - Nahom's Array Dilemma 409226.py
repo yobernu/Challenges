160cee2b73af70a51851a74116cc3ef61d2f78ad
Yoseph Berhanu <yobernu@gmail.com>
2025-11-18 19:15:27 +0300

    Created 'F - Nahom's Array Dilemma' by A2SV Hub

---
+++ A2SV G6 - Round #6 18-Nov-2025/F - Nahom's Array Dilemma 409226.py
@@ -0,0 +1,35 @@
+# Problem: F - Nahom's Array Dilemma - https://codeforces.com/gym/594077/problem/F
+
+import sys
+
+def solve():
+    n = int(sys.stdin.readline().strip())
+    nums = list(map(int, sys.stdin.readline().strip().split()))
+
+    def prevGreater(nums):
+        stack = []
+        pref = [0]
+        
+        for num in nums:
+            pref.append(pref[-1] + num)
+    
+        for i in range(n):
+
+            while stack and nums[stack[-1]] <= nums[i]:
+                v = stack.pop()
+
+                if  pref[i] - pref[v]> 0:
+                    return False
+                
+            stack.append(i)
+
+        return True
+    
+    if prevGreater(nums) and prevGreater(nums[::-1]):
+        return "YES"
+    
+    return "NO"
+
+t = int(sys.stdin.readline().strip())
+for _ in range(t):
+    print(solve())
