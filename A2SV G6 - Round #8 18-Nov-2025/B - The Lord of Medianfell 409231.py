efaf2df232a747a88e69200c288870db09207a3f
Yoseph Berhanu <yobernu@gmail.com>
2025-11-18 19:18:44 +0300

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 18-Nov-2025/B - The Lord of Medianfell 409231.py
@@ -0,0 +1,8 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+t = int(input())
+for _ in range(t):
+    n, s = map(int, input().split())
+    k = (n+2)//2
+    
+    print(s//k)
