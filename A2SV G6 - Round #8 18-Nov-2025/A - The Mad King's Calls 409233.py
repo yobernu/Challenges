1ab80aee0ed05bf2d1299f5e0fa5a882afff537e
Yoseph Berhanu <yobernu@gmail.com>
2025-11-18 19:18:56 +0300

    Created 'A - The Mad King's Calls' by A2SV Hub

---
+++ A2SV G6 - Round #8 18-Nov-2025/A - The Mad King's Calls 409233.py
@@ -0,0 +1,21 @@
+# Problem: A - The Mad King's Calls - https://codeforces.com/gym/599383/problem/A
+
+t = int(input())
+for _ in range(t):
+    num = int(input())
+    ans = []
+    flag = False
+    for i in range(1, 10):
+        for j in range(1, 5):
+            temp = str(i)*j
+            ans.append(int(temp))
+            if int(temp) == num:
+                flag = True
+                break
+        if flag:
+            break
+    res = 0
+    for i in ans:
+        res += len(str(i))
+    print(res)
+
