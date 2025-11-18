e485dfc9f7d1a285aa89a5eaca86fdec289d2911
Yoseph Berhanu <yobernu@gmail.com>
2025-11-18 19:18:37 +0300

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 18-Nov-2025/B - The Lord of Medianfell 409229.py
@@ -0,0 +1,7 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+t = int(input()) 
+for _ in range(t): 
+   n, s = [int(i) for i in input().split()]
+   m = n // 2 + 1 
+   print(s // m) 
